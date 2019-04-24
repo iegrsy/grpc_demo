03591b86966fff28ea9a7c3b8ec600657387a5f7
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2019-04-24 15:51:22 +0300

    Update template.

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # Proto compile and GRPC sample code
-### ***Build***
+
+## *Proto Compile*
+
+Proto dosyalarının derlenmesi ve jar kütüphanesi oluşturulması.
+
 ```sh
-./gradlew build
-./gradlew execute
+./gradlew assemble
 ```
+
+## *GRPC Test*
+
+Bağlantı testi için kafa bilgisi çağrılıyor.  
+
+> rpc GetHeads(google.protobuf.Empty) returns (PtzHeadInfo) {}
+
+```sh
+#./gradlew execute --args='<host> <port>'
+
+./gradlew execute --args='10.5.179.29 50058'
+```
+
+Çalışan kod bloğu
+
+```java
+public static void main(String[] args) {
+    try {
+        if (args.length < 2)
+            throw new InvalidParameterException("Sample use ./gradlew execute --args='<host> <port>'");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(args[0], Integer.parseInt(args[1])).usePlaintext().build();
+        PTZServiceGrpc.PTZServiceBlockingStub blockingStub = PTZServiceGrpc.newBlockingStub(channel);
+
+        Ptzp.PtzHeadInfo headInfo = blockingStub.getHeads(Empty.newBuilder().build());
+        System.out.println(headInfo.toString());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+``` 
--- src/main/java/iegrsy/space/Demo.java
+++ src/main/java/iegrsy/space/Demo.java
@@ -1,6 +1,9 @@
 package iegrsy.space;
 
 import com.google.protobuf.Empty;
+
+import java.security.InvalidParameterException;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import ptzp.PTZServiceGrpc;
@@ -9,10 +12,15 @@ import ptzp.Ptzp;
 public class Demo {
 
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("0.0.0.0", 2222).usePlaintext().build();
-        PTZServiceGrpc.PTZServiceBlockingStub blockingStub = PTZServiceGrpc.newBlockingStub(channel);
         try {
+            if (args.length < 2)
+                throw new InvalidParameterException("Sample use ./gradlew execute --args='<host> <port>'");
+
+            ManagedChannel channel = ManagedChannelBuilder.forAddress(args[0], Integer.parseInt(args[1])).usePlaintext().build();
+            PTZServiceGrpc.PTZServiceBlockingStub blockingStub = PTZServiceGrpc.newBlockingStub(channel);
+
             Ptzp.PtzHeadInfo headInfo = blockingStub.getHeads(Empty.newBuilder().build());
+            System.out.println(headInfo.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
